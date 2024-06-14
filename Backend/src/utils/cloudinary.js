31be42072c2e9318d5673603b1f0260692f7ed88
dhananjay421366 <nimbalkardhananjay349@gmail.com>
2024-06-14 14:13:50 +0530

    adding first middleware multer

---
+++ Backend/src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"; // file system
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      return null;
+    }
+
+    // upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    // file has been uploaded successfully
+    console.log("File is uploaded on cloudinary", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the localy saved temporary file as the upload operation got faield
+
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
